fb0bda326833fe323a4e023b0f94139e19186840
Aashir-Technolligence <aashi7651@gmail.com>
2020-03-18 14:30:22 +0500

    Merge branch 'basit_tech' of https://github.com/Bilal-Technolligence/EmergencyServices into aashir_tech
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/emergencyservices/VictumHelpActivity.java
    #	app/src/main/res/layout/activity_blood_aactivity.xml

--- app/src/main/java/com/example/emergencyservices/AlertHelper.java
+++ app/src/main/java/com/example/emergencyservices/AlertHelper.java
@@ -46,13 +46,13 @@ DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
                     final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
                     final DatabaseReference dref = FirebaseDatabase.getInstance().getReference();
                     final String push = FirebaseDatabase.getInstance().getReference().child("Notification").push().getKey();
-                    dref.child("Notification").child(helper).child(push).child("status").setValue("Unread");
+
                     locationManager = (LocationManager) getSystemService( LOCATION_SERVICE );
                     locationListener = new LocationListener() {
                         @RequiresApi(api = Build.VERSION_CODES.P)
                         @Override
                         public void onLocationChanged(Location location) {
-
+                            dref.child("Notification").child(helper).child(push).child("status").setValue("Unread");
                             dref.child("Notification").child(helper).child(push).child("lon").setValue(String.valueOf(location.getLongitude()));
                             dref.child("Notification").child(helper).child(push).child("lat").setValue(String.valueOf(location.getLatitude()));
                             dref.child("Notification").child(helper).child(push).child("message").setValue(message.getText().toString());
